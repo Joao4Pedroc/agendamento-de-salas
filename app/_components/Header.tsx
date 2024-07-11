ca814c0d9ea2698e9e6413d41ae7a2dd256c8af3
Joao4Pedroc <joao4pedroc@gmail.com>
2024-07-11 14:03:43 -0300

    First Commit

---
+++ app/_components/Header.tsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header className="flex justify-between">
+      <span>fepecs</span>
+      <span className="pr-10">
+        <span className="pr-5">login</span>
+        <span>user img</span>
+      </span>
+    </header>
+  );
+}
+
+export default Header;
