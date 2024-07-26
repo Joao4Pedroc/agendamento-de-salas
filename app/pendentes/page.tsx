fc1dcc4d02a1ab6e3a50e68d6dacfb9b4e89923b
Joao4Pedroc <joao4pedroc@gmail.com>
2024-07-26 14:54:04 -0300

    Feat: page pendentes

--- app/pendentes/page.tsx
+++ app/pendentes/page.tsx
@@ -5,9 +5,12 @@ import { getSalas } from "../_services/apiSalas";
 import { Sala } from "../_components/types";
 import Image from "next/image";
 import useAdminId from "../_Helper/getAdminId";
+import { getAgendamentoPendentes } from "../_services/apiAgendamento";
+import reformatTime from "../_Helper/reformatTime";
 
 function Salas() {
-  const [salas, setSalas] = useState<Sala[]>();
+  const [salas, setSalas] = useState<any>();
+  const [agendamentos, setAgendamentos] = useState<any>();
 
   const { admin, userId } = useAdminId();
 
@@ -25,32 +28,54 @@ function Salas() {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await getAgendamentoPendentes();
+        if (!data) throw new Error("Não foi possivel carregar as informações");
+        setAgendamentos(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
   if (admin) {
     return (
       <div className="grid grid-cols-3 gap-12 mt-10">
-        {salas?.map((sala) => (
-          <ul className="font-medium flex gap-4 " key={sala.id}>
-            <li className="">
-              <Image
-                className="rounded-lg"
-                src={sala.imagem}
-                height={250}
-                width={250}
-                alt={`Imagem da sala ${sala.nome}`}
-              />
-            </li>
-            <div>
-              <li>Sala {sala.nome}</li>
+        {agendamentos?.map((agendamento) => {
+          const sala = salas.filter((sala) => sala.id === agendamento.idSala);
 
-              <li className="">Capacidade: {sala.capacidade} pessoas</li>
-              <li>{sala.televisão ? "Televisão" : "Não possui televisão"}</li>
-              <li>{sala.internet ? "Internet" : "Não possui internet"}</li>
-              <li className="text-blue-900 ">
-                <a href={`/salas/${sala.id}`}>Ver calendario da sala</a>
+          return (
+            <ul className="font-medium flex gap-4 " key={agendamento.id}>
+              <li className="">
+                <Image
+                  className="rounded-lg"
+                  src={sala[0].imagem}
+                  height={250}
+                  width={250}
+                  alt={`Imagem da sala ${sala[0].nome}`}
+                />
               </li>
-            </div>
-          </ul>
-        ))}
+              <div>
+                <li>Sala {sala[0].nome}</li>
+
+                <li className="">
+                  Horario entrada: {reformatTime(agendamento.horarioEntrada)}
+                </li>
+                <li>
+                  Horario entrada: {reformatTime(agendamento.horarioSaida)}
+                </li>
+                <li>Agenda-da por: {agendamento.nomeUsuario}</li>
+                <li className="text-blue-900 ">
+                  <a href={`/salas/${sala[0].id}`}>Ver calendario da sala</a>
+                </li>
+              </div>
+            </ul>
+          );
+        })}
       </div>
     );
   } else
